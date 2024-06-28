5fec6564a92a3eb4c406173f2b18de1bad6a3984
Negus <natnaelbirhanu22@gmail.com>
2024-06-28 20:19:00 +0300

    values: in go simmilar with other languages there are strings, integers, and booleans...

--- hello.go
+++ hello.go
@@ -5,5 +5,29 @@ import "fmt"
 
 func main(){
 
-	fmt.Println("Starting Go");
+	// hello()
+	// value()
+
+
+	
 }
+
+func hello(){
+	fmt.Println("Hello world");
+}
+
+func value(){
+
+	fmt.Println("Go" + " " + "String concatination")
+
+	fmt.Print("no new line?")
+	fmt.Print(" Yeah, this will be just next to the 'no new line' string!")
+	fmt.Println("1+1 = " , 1+1)
+
+	fmt.Println(7.0/3.0)
+
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
